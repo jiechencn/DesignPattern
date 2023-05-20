47c0607c7788499ce305314eed2c73cb7fdf55a8
Jie Chen <jiechencn@qq.com>
2023-05-20 20:59:28 +0800

    proxy

---
+++ Proxy/IInternet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public interface IInternet
+    {
+        public void Connect(string url);
+    }
+
+    public class RealInternet : IInternet
+    {
+        public void Connect(string url)
+        {
+            Console.WriteLine("connected to " + url);
+        }
+    }
+
+    public class ProxyInternet: IInternet
+    {
+        private IInternet realInternet = new RealInternet();
+        public void Connect(string url)
+        {
+            RejectBadUrl(url);
+            realInternet.Connect(url);
+            LogAccess(url);
+        }
+
+        private void LogAccess(string url)
+        {
+            Console.WriteLine("someone is accessing " + url);
+        }
+
+        private void RejectBadUrl(string url)
+        {
+            // 检查有害网址，抛异常;
+            if (url.Contains("bad"))
+            {
+                throw new Exception("bad url");
+            }
+        }
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Proxy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IInternet internet = new ProxyInternet();
+            internet.Connect("https://google.com");
+
+            internet.Connect("https://bad.com");
+        }
+    }
+}
